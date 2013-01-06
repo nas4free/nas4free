6c87e5cec3e58df3b5e0035b7c9de0f81391e2e4
zoon01 <zoon01@users.noreply.github.com>
2013-01-06 17:17:51 +0000

    Revert to mDNSResponder 333.10.

--- build/ports/mDNSResponder/files/patch-mDNSCore_mDNS.c
+++ build/ports/mDNSResponder/files/patch-mDNSCore_mDNS.c
@@ -1,10 +1,10 @@
---- mDNSCore/mDNS.c.orig	2012-08-30 01:27:16.000000000 +0200
-+++ mDNSCore/mDNS.c	2012-12-20 04:12:17.000000000 +0100
-@@ -12665,6 +12665,7 @@
+--- mDNSCore/mDNS.c.orig	2011-08-04 07:06:51.000000000 +0900
++++ mDNSCore/mDNS.c	2011-10-02 08:49:51.000000000 +0900
+@@ -11412,6 +11412,7 @@
  
-     // If any deregistering records remain, send their deregistration announcements before we exit
-     if (m->mDNSPlatformStatus != mStatus_NoError) DiscardDeregistrations(m);
+ 	// If any deregistering records remain, send their deregistration announcements before we exit
+ 	if (m->mDNSPlatformStatus != mStatus_NoError) DiscardDeregistrations(m);
 +	else if (m->ResourceRecords) SendResponses(m);
  
-     mDNS_Unlock(m);
+ 	mDNS_Unlock(m);
  
