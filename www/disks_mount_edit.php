698ce911c42dd5b6c6f187a1557783a9f9127a65
zoon01 <zoon01@users.noreply.github.com>
2013-04-14 04:58:40 +0000

    Fix hast to mount points. (br 85)

--- www/disks_mount_edit.php
+++ www/disks_mount_edit.php
@@ -8,16 +8,16 @@
 	
 	Portions of freenas (http://www.freenas.org).
 	Copyright (c) 2005-2011 by Olivier Cochard <olivier@freenas.org>.
-	All rights reserved.	
+	All rights reserved.
 
 	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions are met: 
+	modification, are permitted provided that the following conditions are met:
 
 	1. Redistributions of source code must retain the above copyright notice, this
-	   list of conditions and the following disclaimer. 
+	   list of conditions and the following disclaimer.
 	2. Redistributions in binary form must reproduce the above copyright notice,
 	   this list of conditions and the following disclaimer in the documentation
-	   and/or other materials provided with the distribution. 
+	   and/or other materials provided with the distribution.
 
 	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -31,7 +31,7 @@
 	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 	The views and conclusions contained in the software and documentation are those
-	of the authors and should not be interpreted as representing official policies, 
+	of the authors and should not be interpreted as representing official policies,
 	either expressed or implied, of the NAS4Free Project.
 */
 require("auth.inc");
@@ -231,7 +231,7 @@ if ($_POST) {
 		if ($_POST['partitiontype'] != 'p') {
 			$input_errors[] = gettext("HAST volume can be use with GPT/UFS only.");
 		}
-		$device = "{$_POST['hvol']}{$_POST['partition']}";
+		$device = "{$_POST['hvol']}";
 		if ($device === $cfdevice) {
 			$input_errors[] = gettext("Can't mount the system partition 1, the DATA partition is the 2.");
 		}
