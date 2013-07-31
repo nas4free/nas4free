9d3f5d41d1c89562e72beb2ec68bb3da62b9821b
redantz <redantz@beaaabd7-781f-4c1e-bc4d-5cfe25df7511>
2013-07-31 01:41:42 +0000

    Add driver for ARECA SATA/SAS HBA Controller ARC1320, ARC1300 Note: This is not loaded by default. In embedded/full install, add arcsas_load="YES" via System|Advanced|loader.conf to load on boot.
    Thank you Areca to allow us to include this into NAS4Free distribution.

--- experimental/9.1.0.1/trunk/build/make.sh
+++ experimental/9.1.0.1/trunk/build/make.sh
@@ -344,7 +344,21 @@ build_kernel() {
 				modulesdir=${NAS4FREE_OBJDIRPREFIX}/usr/src/sys/${NAS4FREE_KERNCONF}/modules/usr/src/sys/modules;
 				for module in $(cat ${NAS4FREE_WORKINGDIR}/modules.files | grep -v "^#"); do
 					install -v -o root -g wheel -m 555 ${modulesdir}/${module} ${NAS4FREE_ROOTFS}/boot/kernel
-				done;;
+				done
+
+				echo "--------------------------------------------------------------";
+				echo ">>> Add ${NAS4FREE_ARCH} Specific Kernel Modules";
+				echo "--------------------------------------------------------------";
+
+				[ -f ${NAS4FREE_WORKINGDIR}/modules_${NAS4FREE_ARCH}.files ] && rm -f ${NAS4FREE_WORKINGDIR}/modules_${NAS4FREE_ARCH}.files;
+				cp -f ${NAS4FREE_SVNDIR}/build/kernel-config/modules_${NAS4FREE_ARCH}.files ${NAS4FREE_WORKINGDIR};
+
+				modulesdir=${NAS4FREE_SVNDIR}/build/kernel-modules;
+
+				for module in $(cat ${NAS4FREE_WORKINGDIR}/modules_${NAS4FREE_ARCH}.files | grep -v "^#"); do
+					install -v -o root -g wheel -m 555 ${modulesdir}/${module} ${NAS4FREE_ROOTFS}/boot/modules
+				done
+				;;
   	esac
   done
 
@@ -429,6 +443,23 @@ copy_kmod() {
 		b=`basename ${f}`
 		(cd ${NAS4FREE_OBJDIRPREFIX}/usr/src/sys/${NAS4FREE_KERNCONF}/modules/usr/src/sys/modules; install -v -o root -g wheel -m 555 ${f} $NAS4FREE_TMPDIR/boot/kernel/${b}; gzip -9 $NAS4FREE_TMPDIR/boot/kernel/${b})
 	done
+
+	echo "Copy ${NAS4FREE_ARCH} specific kmod to $NAS4FREE_TMPDIR/boot/modules"
+
+	[ -f ${NAS4FREE_WORKINGDIR}/modules_${NAS4FREE_ARCH}.files ] && rm -f ${NAS4FREE_WORKINGDIR}/modules_${NAS4FREE_ARCH}.files;
+	cp -f ${NAS4FREE_SVNDIR}/build/kernel-config/modules_${NAS4FREE_ARCH}.files ${NAS4FREE_WORKINGDIR};
+
+	if [ ! -d $NAS4FREE_TMPDIR/boot/modules ]; then
+		mkdir -pv $NAS4FREE_TMPDIR/boot/modules
+	fi
+
+	modulesdir=${NAS4FREE_SVNDIR}/build/kernel-modules;
+
+	for module in $(cat ${NAS4FREE_WORKINGDIR}/modules_${NAS4FREE_ARCH}.files | grep -v "^#"); do
+		b=`basename ${modulesdir}/${module}`
+		( install -v -o root -g wheel -m 555 ${modulesdir}/${module} $NAS4FREE_TMPDIR/boot/modules/${b}; gzip -9 $NAS4FREE_TMPDIR/boot/modules/${b})
+	done
+
 	return 0;
 }
 
