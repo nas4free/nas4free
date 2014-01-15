8bcbdc7ae81cc3e56ed7fc40ce253d6a909c2d97
zoon01 <zoon01@users.noreply.github.com>
2014-01-15 02:05:21 +0000

    Upgrade e2fsprogs to 1.42.9.

--- build/ports/e2fsprogs/files/fsck_ext2fs.c
+++ build/ports/e2fsprogs/files/fsck_ext2fs.c
@@ -4,7 +4,7 @@
  * redistributable in accordance with the
  * GNU General Public License v2
  *
- * $FreeBSD: head/sysutils/e2fsprogs/files/fsck_ext2fs.c 300897 2012-07-14 14:29:18Z beat $
+ * $FreeBSD: sysutils/e2fsprogs/files/fsck_ext2fs.c 300897 2012-07-14 14:29:18Z beat $
  *
  * Upstream: $Id: fsck_ext2fs.c,v 1.6 2006/07/02 11:37:49 emma Exp $
  *
