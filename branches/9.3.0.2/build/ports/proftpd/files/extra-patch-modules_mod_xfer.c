dd22c2de0a17fb0f83ca49c45c8443e4cc419526
zoon01 <zoon01@beaaabd7-781f-4c1e-bc4d-5cfe25df7511>
2015-06-10 10:22:44 +0000

    Upgrade proftpd to 1.3.5a on 9.3 too

--- branches/9.3.0.2/build/ports/proftpd/files/extra-patch-modules_mod_xfer.c
+++ branches/9.3.0.2/build/ports/proftpd/files/extra-patch-modules_mod_xfer.c
@@ -1,7 +1,7 @@
---- modules/mod_xfer.c.org	Tue Jan  1 12:31:32 2008
-+++ modules/mod_xfer.c	Tue Jan  1 16:45:20 2008
-@@ -42,6 +42,8 @@
- #include <regex.h>
+--- modules/mod_xfer.c.orig	2015-05-28 02:25:54.000000000 +0200
++++ modules/mod_xfer.c	2015-06-10 08:47:38.000000000 +0200
+@@ -43,6 +43,8 @@
+ # define PRIO_MAX	20
  #endif
  
 +#include "mod_clamav.h"
@@ -9,7 +9,7 @@
  extern module auth_module;
  extern pid_t mpid;
  
-@@ -1582,6 +1584,11 @@
+@@ -1838,6 +1840,11 @@
        return PR_ERROR(cmd);
      }
  
@@ -17,7 +17,7 @@
 +		pr_data_close(FALSE);
 +		return PR_ERROR(cmd);
 +	}
-+	
++
      if (session.xfer.path &&
          session.xfer.path_hidden) {
        if (pr_fsio_rename(session.xfer.path_hidden, session.xfer.path) != 0) {
